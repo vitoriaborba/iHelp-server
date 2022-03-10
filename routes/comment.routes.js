8cc07405c51359f2bca585e9d0c7b3ba9b72258a
Vitoria Borba <vitoriaborba@icloud.com>
2022-03-10 18:07:36 +0000

    Token secret added

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,6 +1,4 @@
 const router = require("express").Router();
-
-const User = require('../models/User.model');
 const Post = require('../models/Post.model');
 const Comment = require('../models/Comment.model');
 
